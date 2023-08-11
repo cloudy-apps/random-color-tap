6d3e25c950d674e2275a1798e8575ff9c4a3750b
Darlan Alves <me@darlanalv.es>
2023-08-11 15:13:00 +0200

    chore: log requests

--- server/index.mjs
+++ server/index.mjs
@@ -1,30 +1,37 @@
 async function changeColor(deviceId) {
   const path = `/v1.0/devices/${deviceId}/commands`;
   const hue = Math.floor(Math.random() * 1000) % 360;
-  const payload = {
+  const lampPayload = {
     commands: [
       { code: "switch_led", value: true },
       { code: "colour_data_v2", value: { h: hue, s: 1000, v: 1000 } },
     ],
   };
-
-  const response = await fetch(
+  const url =
     "https://tuya-connector.jsfn.run/request?method=POST&path=" +
-      encodeURIComponent(path),
-    {
-      mode: "cors",
-      body: JSON.stringify(payload),
-    }
-  );
+    encodeURIComponent(path);
+
+  const payload = {
+    mode: "cors",
+    body: JSON.stringify(lampPayload),
+  };
+
+  console.log(url, payload);
+  const response = await fetch(url, payload);
 
   return response.ok;
 }
 
-export default function (req, res, next) {
+export default async function (req, res, next) {
   if (req.url.startsWith("/tap/")) {
     const deviceId = req.url.replace("/tap/", "");
-    changeColor(deviceId);
-    res.end("");
+    try {
+      await changeColor(deviceId);
+      res.end("");
+    } catch (error) {
+      console.log(error);
+      res.writeHead(500).end();
+    }
   }
 
   next();
